7b8a41a0416baa4b09342d1866c74f325002ea8b
LeonardoScalabrini <leonardo_scalabrini@hotmail.com>
2021-09-08 12:28:06 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,30 @@ terraform {
   }
 }
 
+variable "project_id" {
+  type = string
+}
+
+variable "private_key_id" {
+  type = string
+}
+
+variable "private_key" {
+  type = string
+}
+
+variable "client_email" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_x509_cert_url" {
+  type = string
+}
+
 provider "google" {
   credentials = {
     "type": "service_account",
