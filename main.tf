2ab8a1aa3f7652788b08af68e051f75ee6851b97
LeonardoScalabrini <leonardo_scalabrini@hotmail.com>
2021-09-08 12:13:27 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,18 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("ecstatic-bounty-323023-e527165d2e09.json")
+  credentials = {
+    "type": "service_account",
+    "project_id": var.project_id,
+    "private_key_id": var.private_key_id,
+    "private_key": var.private_key,
+    "client_email": var.client_email,
+    "client_id": var.client_id,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": var.client_x509_cert_url
+  }
 
   project = "ecstatic-bounty-323023"
   region  = "us-central1"
