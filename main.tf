f9ca1489b5dc280e9662e2f86c47eeaadca0e42d
LeonardoScalabrini <leonardo_scalabrini@hotmail.com>
2021-09-08 14:24:21 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,32 +7,12 @@ terraform {
   }
 }
 
-variable "project_id" {
-  type = string
-}
-
-variable "private_key_id" {
-  type = string
-}
-
-variable "private_key" {
-  type = string
-}
-
-variable "client_email" {
-  type = string
-}
-
-variable "client_id" {
-  type = string
-}
-
-variable "client_x509_cert_url" {
+variable "cloud_credential" {
   type = string
 }
 
 provider "google" {
-  credentials = var.project_id
+  credentials = var.cloud_credential
 
   project = "ecstatic-bounty-323023"
   region  = "us-central1"
