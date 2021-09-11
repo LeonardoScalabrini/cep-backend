322d3f5cc9b086b9ea82332fadbe1763079755ce
Leonardo Scalabrini <leonardo.scalabrini@esx.com.br>
2021-09-11 16:32:39 -0300

    Ajustes deploy

--- src/route/get-cep-route.js
+++ src/route/get-cep-route.js
@@ -9,7 +9,7 @@ module.exports = app => {
           res.status(200).json(docs)
         })
       } else {
-        res.status(200).json('Aqui 3')
+        res.status(200).json('Aqui 4')
       }
     })
 }
