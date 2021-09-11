3589e010151a2f931daae9b9b6b56430ad60ba07
Leonardo Scalabrini <leonardo.scalabrini@esx.com.br>
2021-09-11 17:08:02 -0300

    Ajustes deploy

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose')
 var { database } = require('./config.js')
 
+console.log('database', JSON.stringify(database))
+console.log('database', database.stringConnection)
+
 if (!database.stringConnection) {
   return
 }
--- src/route/get-cep-route.js
+++ src/route/get-cep-route.js
@@ -9,7 +9,7 @@ module.exports = app => {
           res.status(200).json(docs)
         })
       } else {
-        res.status(200).json('Aqui 4')
+        res.status(200).json('Aqui 5')
       }
     })
 }
