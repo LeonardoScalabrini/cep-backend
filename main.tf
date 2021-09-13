4b1d78211e0097b74d09ac0744578ef330160e40
Leonardo Scalabrini <leonardo.scalabrini@esx.com.br>
2021-09-13 20:32:00 -0300

    Ajustes deploy

--- main.tf
+++ main.tf
@@ -43,64 +43,21 @@ provider "google" {
   zone    = var.zone
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = var.zone
 
-resource "google_compute_address" "static" {
-  name = "ipv4-address"
-}
-
-data "google_compute_image" "container-optimized-image" {
-  family = "cos-81-lts"
-  project = "cos-cloud"
-}
-
-module "gce-container" {
-  source = "github.com/terraform-google-modules/terraform-google-container-vm"
-
-  container = {
-    image = var.image
-  }
-
-  restart_policy = "Always"
-}
-
-resource "google_compute_instance" "instance_with_ip" {
-  name         = var.name
-  machine_type = "e2-medium"
-  zone         = var.zone
-  tags = ["http-server"]
-
-  metadata = {
-    gce-container-declaration = module.gce-container.metadata_value
-    google-logging-enabled    = "true"
-    google-monitoring-enabled = "true"
-    startup-script-url = var.startup_script
-  }
-
-  service_account {
-      email  = "1001547212506-compute@developer.gserviceaccount.com"
-      scopes = [
-        "https://www.googleapis.com/auth/devstorage.read_only",
-        "https://www.googleapis.com/auth/logging.write",
-        "https://www.googleapis.com/auth/monitoring.write",
-        "https://www.googleapis.com/auth/servicecontrol",
-        "https://www.googleapis.com/auth/service.management.readonly",
-        "https://www.googleapis.com/auth/trace.append"
-      ]
-  }
-
-  boot_disk {
-    initialize_params {
-      image = data.google_compute_image.container-optimized-image.self_link
+  template {
+    spec {
+      containers {
+        image = var.image
+        containerPort = 80
+      }
     }
   }
 
-  network_interface {
-    network = "default"
-    access_config {
-      nat_ip = google_compute_address.static.address
-    }
+  traffic {
+    percent = 100
+    latest_revision = true
   }
 }
