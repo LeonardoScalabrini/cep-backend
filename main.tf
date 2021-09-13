ea96a11b97f303b16554e450797bebed2b3474c5
Leonardo Scalabrini <leonardo.scalabrini@esx.com.br>
2021-09-12 23:07:13 -0300

    Ajustes deploy

--- main.tf
+++ main.tf
@@ -71,6 +71,7 @@ resource "google_compute_instance" "instance_with_ip" {
   machine_type = "e2-medium"
   zone         = var.zone
   tags = ["http-server"]
+  metadata_startup_script = var.startup_script
 
   metadata = {
     gce-container-declaration = module.gce-container.metadata_value
@@ -102,8 +103,4 @@ resource "google_compute_instance" "instance_with_ip" {
       nat_ip = google_compute_address.static.address
     }
   }
-
-  lifecycle {
-        ignore_changes = [metadata_startup_script]
-    }
 }
