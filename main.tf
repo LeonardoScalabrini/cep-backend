e09de812db5acf2a8cc42d4bf73963523497946a
Leonardo Scalabrini <leonardo.scalabrini@esx.com.br>
2021-09-13 23:11:45 -0300

    Ajustes deploy

--- main.tf
+++ main.tf
@@ -47,8 +47,8 @@ resource "google_project_service" "run_api" {
   service = "run.googleapis.com"
 }
 
-resource "google_cloud_run_service" "run_service" {
-  name     = "run_service"
+resource "google_cloud_run_service" "cep-backend" {
+  name     = "cep-backend"
   location = var.region
 
   template {
@@ -71,12 +71,12 @@ resource "google_cloud_run_service" "run_service" {
 }
 
 resource "google_cloud_run_service_iam_member" "run_all_users" {
-  service  = google_cloud_run_service.run_service.name
-  location = google_cloud_run_service.run_service.location
+  service  = google_cloud_run_service.cep-backend.name
+  location = google_cloud_run_service.cep-backend.location
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
 
 output "service_url" {
-  value = google_cloud_run_service.run_service.status[0].url
+  value = google_cloud_run_service.cep-backend.status[0].url
 }
