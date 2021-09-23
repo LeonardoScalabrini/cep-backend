1a59e92e55621fd7fdcde01f80a0fd793e7396a3
Leonardo Scalabrini <leonardo.scalabrini@esx.com.br>
2021-09-23 18:25:25 -0300

    Ajustado variables.tf

--- variable.tf
+++ variable.tf
@@ -1,19 +1,24 @@
 variable "project_id" {
-  type = string
+  type    = string
+  default = "project_id"
 }
 
 variable "region" {
   type = string
+  default = "region"
 }
 
 variable "zone" {
   type = string
+  default = "zone"
 }
 
 variable "image" {
   type = string
+  default = "image"
 }
 
 variable "cloud_credential" {
   type = string
+  default = "cloud_credential"
 }
