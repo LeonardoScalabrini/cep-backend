4864e413f7c0a839de91ae3ca958338134f84130
Leonardo Scalabrini <leonardo.scalabrini@esx.com.br>
2022-03-17 09:32:21 -0300

    Revert "Implementado proxy"
    This reverts commit d2413227d2d8bc9259ab42c89938227d5e44affd.

--- src/config/express.js
+++ src/config/express.js
@@ -1,6 +1,5 @@
 const { PORT } = require('./config.js')
 const helmet = require('helmet')
-const expressSession = require('express-session')
 const express = require('express')
 const bodyParser = require('body-parser')
 const ALLOWED_METHODS = ['GET', 'POST']
@@ -8,14 +7,6 @@ const ALLOWED_METHODS = ['GET', 'POST']
 module.exports = () => {
   const app = express()
   app.use(helmet())
-  app.set('trust proxy', 1)
-  app.use(expressSession({
-    resave: true,
-    saveUninitialized: true,
-    secret : 's3Cur3',
-    name : 'sessionId',
-    })
-  );
   app.use((req, res, next) => {
     res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
     if (!ALLOWED_METHODS.includes(req.method)) 
--- test/route/method-not-allowed-test.js
+++ test/route/method-not-allowed-test.js
@@ -6,7 +6,7 @@ describe('OPTIONS *', () => {
 
   it('responds with json', () => {
     request(express)
-      .put('/')
+      .options('/')
       .expect(405)
       .end(function (err, res) {
         assert.equal(res.body, 'Método não permitido!')
