7a054d473e5f48d3bb7af27d1d918e8522920301
Leonardo Scalabrini <leonardo.scalabrini@esx.com.br>
2022-03-16 22:01:08 -0300

    Adicionado helmet

--- src/config/express.js
+++ src/config/express.js
@@ -1,19 +1,13 @@
 const { PORT } = require('./config.js')
+const helmet = require('helmet')
 const express = require('express')
 const bodyParser = require('body-parser')
 
 module.exports = () => {
   const app = express()
+  app.use(helmet())
   app.use(bodyParser.json())
   app.set('port', PORT)
-
-  app.use(function (req, res, next) {
-    res.setHeader(
-      'Content-Security-Policy',
-      "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
-    );
-    next();
-  });
   require('./db')
   require('../route/get-cep-route')(app)
   require('../route/post-cep-route')(app)
